34e43f91dd2e957a944a5d7d64fe4fc36cc7e91b
AkzholS7 <suborovakzhol7@gmail.com>
2023-11-26 20:06:19 +0600

    tried to work on functions

---
+++ sixth_day.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe
+
+def greeting(fname,lname,state):
+	print("Hello, %s %s! How is the weather in %s?" % (fname, lname, state))
+
+first_name = "Peter"
+last_name = "Gervase"
+mystate = "North Carolina"
+
+greeting(first_name, last_name, mystate)
