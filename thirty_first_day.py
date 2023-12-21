3cbd9a61549531b78c9410bde480c8a136472a28
AkzholS7 <suborovakzhol7@gmail.com>
2023-12-21 20:52:24 +0600

    completed

---
+++ thirty_first_day.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe
+
+def congratulate_customer(customer_name):
+    message = f"Congratulations, {customer_name}, YOU did THIS!!!"
+    print(message)
+
+
+customer = input("Enter customer's name: ")
+
+
+congratulate_customer(customer)
